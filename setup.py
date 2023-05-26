18855c65dab02ed32fa33dc0f96b8cb147630c9f
Dani Jordan <dani.jordan@healthvana.com>
2023-05-26 17:48:29 -0400

    update setup.py for django 4.2

--- setup.py
+++ setup.py
@@ -262,6 +262,7 @@ setup(
         "Framework :: Django :: 3.2",
         "Framework :: Django :: 4.0",
         "Framework :: Django :: 4.1",
+        "Framework :: Django :: 4.2",
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
         "Development Status :: 4 - Beta",
